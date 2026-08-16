05e4e24c85b91e7d2c07b31f9210bf1a3e56f0a4
kumartj <tarun.urmate@gmail.com>
2026-08-16 14:12:47 -0400

    feat: bootable reference-data-service skeleton on port 8070

---
+++ src/main/java/com/ccb/referencedata/ReferenceDataApplication.java
@@ -0,0 +1,12 @@
+package com.ccb.referencedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReferenceDataApplication.class, args);
+    }
+}
